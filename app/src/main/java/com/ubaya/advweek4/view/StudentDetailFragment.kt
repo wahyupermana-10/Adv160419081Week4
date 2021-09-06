ce56a1f98a5616102f556c041b39d8d438f4e9d6
Risqi <wahyupermana51@gmail.com>
2021-09-06 14:46:49 +0900

    init commit

---
+++ app/src/main/java/com/ubaya/advweek4/view/StudentDetailFragment.kt
@@ -0,0 +1,41 @@
+package com.ubaya.advweek4.view
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.ubaya.advweek4.R
+import com.ubaya.advweek4.model.Student
+import com.ubaya.advweek4.viewmodel.DetailViewModel
+import com.ubaya.advweek4.viewmodel.ListViewModel
+import kotlinx.android.synthetic.main.fragment_student_detail.*
+import kotlinx.android.synthetic.main.fragment_student_list.*
+
+class StudentDetailFragment : Fragment() {
+    private lateinit var viewModel: DetailViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_student_detail, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel = ViewModelProvider(this).get(DetailViewModel::class.java)
+        viewModel.fetch()
+
+        viewModel.studentLD.observe(viewLifecycleOwner, Observer {
+            txtID.setText(it.id)
+            txtName.setText(it.name)
+            txtBod.setText(it.bod)
+            txtPhone.setText(it.phone)
+        })
+    }
+}
---
+++ app/src/main/java/com/ubaya/advweek4/view/StudentListFragment.kt
@@ -0,0 +1,64 @@
+package com.ubaya.advweek4.view
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.ubaya.advweek4.R
+import com.ubaya.advweek4.viewmodel.ListViewModel
+import kotlinx.android.synthetic.main.fragment_student_detail.*
+import kotlinx.android.synthetic.main.fragment_student_list.*
+
+
+class StudentListFragment : Fragment() {
+    private lateinit var viewModel: ListViewModel
+    private val studentListAdapter = StudentListAdapter(arrayListOf())
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_student_list, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        viewModel = ViewModelProvider(this).get(ListViewModel::class.java)
+        viewModel.refresh()
+        recView.layoutManager = LinearLayoutManager(context)
+        recView.adapter = studentListAdapter
+        observeViewModel()
+    }
+
+    fun observeViewModel() {
+        viewModel.studentsLD.observe(viewLifecycleOwner, Observer {
+            studentListAdapter.updateStudentList(it)
+        })
+
+        viewModel.loadingErrorLD.observe(viewLifecycleOwner, Observer {
+            if(it == true) {
+                txtError.visibility = View.VISIBLE
+            } else {
+                txtError.visibility = View.GONE
+            }
+        })
+
+        viewModel.loadingDoneLD.observe(viewLifecycleOwner, Observer {
+            if(it == true) {
+                recView.visibility = View.GONE
+                progressLoad.visibility = View.VISIBLE
+            } else {
+                recView.visibility = View.VISIBLE
+                progressLoad.visibility = View.GONE
+            }
+        })
+
+
+    }
+}
