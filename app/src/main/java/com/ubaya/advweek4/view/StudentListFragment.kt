26792245ce9ffae053809729fe3ec0eab77546ff
Risqi <wahyupermana51@gmail.com>
2021-09-09 15:26:01 +0900

    Gson Volley

--- app/src/main/java/com/ubaya/advweek4/view/StudentListFragment.kt
+++ app/src/main/java/com/ubaya/advweek4/view/StudentListFragment.kt
@@ -33,6 +33,15 @@ class StudentListFragment : Fragment() {
         viewModel.refresh()
         recView.layoutManager = LinearLayoutManager(context)
         recView.adapter = studentListAdapter
+
+        refreshLayout.setOnRefreshListener {
+            recView.visibility = View.GONE
+            txtError.visibility = View.GONE
+            progressLoad.visibility = View.VISIBLE
+            viewModel.refresh()
+            refreshLayout.isRefreshing = false
+        }
+
         observeViewModel()
     }
 
@@ -49,7 +58,7 @@ class StudentListFragment : Fragment() {
             }
         })
 
-        viewModel.loadingDoneLD.observe(viewLifecycleOwner, Observer {
+        viewModel.loadingLD.observe(viewLifecycleOwner, Observer {
             if(it == true) {
                 recView.visibility = View.GONE
                 progressLoad.visibility = View.VISIBLE
--- app/src/main/java/com/ubaya/advweek4/viewmodel/ListViewModel.kt
+++ app/src/main/java/com/ubaya/advweek4/viewmodel/ListViewModel.kt
@@ -1,28 +1,55 @@
 package com.ubaya.advweek4.viewmodel
 
+import android.app.Application
+import android.util.Log
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.android.volley.Request
+import com.android.volley.RequestQueue
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
 import com.ubaya.advweek4.model.Student
 
-class ListViewModel:ViewModel() {
+class ListViewModel(application: Application):AndroidViewModel(application) {
     val studentsLD = MutableLiveData<List<Student>>()
     val loadingErrorLD = MutableLiveData<Boolean>()
-    val loadingDoneLD = MutableLiveData<Boolean>()
+    val loadingLD = MutableLiveData<Boolean>()
+
+    private var TAG = "volleyTag"
+    private var queue:RequestQueue ?= null
+
+
 
     fun refresh() {
-        val student1 =
-            Student("16055","Nonie","1998/03/28","5718444778","http://dummyimage.com/75x100.jpg/cc0000/ffffff")
-
-        val student2 =
-            Student("13312","Rich","1994/12/14","3925444073","http://dummyimage.com/75x100.jpg/5fa2dd/ffffff")
-
-        val student3 =
-            Student("11204","Dinny","1994/10/07","6827808747","http://dummyimage.com/75x100.jpg/5fa2dd/ffffff1")
-        val studentList:ArrayList<Student> = arrayListOf<Student>(student1, student2, student3)
-
-        studentsLD.value = studentList
         loadingErrorLD.value = false
+        loadingLD.value = false
 
-        loadingDoneLD.value = false
+        queue = Volley.newRequestQueue(getApplication())
+        var url = "http://adv.jitusolution.com/student.php"
+
+        val stringRequest = StringRequest(Request.Method.GET, url,
+            { response ->
+                val sType = object : TypeToken<List<Student>>(){}.type
+                val result = Gson().fromJson<List<Student>>(response, sType)
+                studentsLD.value = result
+
+                loadingLD.value=false
+                Log.d("showvolly", response.toString())
+            },
+            {
+                loadingLD.value=false
+                loadingErrorLD.value=false
+                Log.d("Showvolley",it.toString())
+            })
+        stringRequest.tag = TAG
+        queue?.add(stringRequest)
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        queue?.cancelAll(TAG)
     }
 }
